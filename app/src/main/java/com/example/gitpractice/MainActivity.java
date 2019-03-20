4c8215ee5d8ae7ca69d85780b73ea5c4c5d6244d
KIMJEEHYUN <0803rlawlgus@gmail.com>
2019-03-20 15:17:06 +0900

    Initial Commit

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -11,3 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
